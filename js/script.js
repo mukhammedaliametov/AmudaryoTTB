9b52885002fb2612b3f134655a518574585f2a32
Mukhammedali Ametov <ametovmukhammedali07@gmail.com>
2023-08-05 10:37:51 +0500

    create navbar and home page :)

--- js/script.js
+++ js/script.js
@@ -2,8 +2,20 @@
 
 const navEl = document.querySelector('.navigation');
 const buttonEl = document.querySelector('.hamburger');
+const headerEl = document.querySelector('.second-header')
 
 buttonEl.addEventListener('click', () => {
   navEl.classList.toggle('active');
   buttonEl.classList.toggle('active');
-})
+});
+
+window.onscroll = function () { myFunction() };
+var sticky = headerEl.offsetTop;
+
+function myFunction() {
+  if (window.scrollY >= 1) {
+    headerEl.classList.add("sticky");
+  } else {
+    headerEl.classList.remove("sticky");
+  }
+}
